fc11ca1626026acfb2f4d95bfa115ce99533d420
Vladaster <vladmuravin@yandex.ru>
2023-02-26 22:01:21 -0800

    Вторая задача

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Введите первое число A - ");
+﻿// Задача 2
+/*
+Console.Write("Введите первое число A - ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число b - ");
 int b = Convert.ToInt32(Console.ReadLine());
@@ -11,4 +13,18 @@ if(a>b)
 else if(a<b)
 {
     Console.WriteLine("Число B больше числа A");
-}
+} */
+
+// Задача 4
+Console.Write("Введите первое число - ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число - ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число - ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+Console.Write($"Максимальное число - {max}");
