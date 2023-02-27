8f96802f25591a99f21f77da7c984c5eec4de2f2
Vladaster <vladmuravin@yandex.ru>
2023-02-26 22:17:10 -0800

    Третья задача

--- Program.cs
+++ Program.cs
@@ -15,6 +15,8 @@ else if(a<b)
     Console.WriteLine("Число B больше числа A");
 } */
 
+
+/*
 // Задача 4
 Console.Write("Введите первое число - ");
 int a = Convert.ToInt32(Console.ReadLine());
@@ -27,4 +29,11 @@ int max = a;
 
 if(b > max) max = b;
 if(c > max) max = c;
-Console.Write($"Максимальное число - {max}");
+Console.Write($"Максимальное число - {max}"); */
+
+Console.Write("Введите число - ");
+int a = Convert.ToInt32(Console.Read());
+if (a % 2 == 0) Console.Write("Четное");
+else Console.WriteLine("Нечетное");
+// на счет этой задачи есть сомнения, у меня числа 12 и 14 нечетные получаются
+// с 8 задачей у меня проблемы, я не понял пока что как ее сделать 
